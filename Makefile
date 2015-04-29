71048199277046ba84e61f6b6c08f548b9a5f5e8
Hans Tang <htang014@hammer.cs.ucr.edu>
2015-04-29 01:47:28 -0700

    Nearly completed -l. Update makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CFLAGS = -ansi -pedantic -Wall -Werror
 
-all: rshell cp
+all: rshell cp ls
 	g++ $(CFLAGS) src/rshell.cpp -o bin/rshell
 	
 
@@ -9,4 +9,7 @@ rshell:
 	g++ $(CFLAGS) src/rshell.cpp -o bin/rshell
 
 cp:
-	g++ $(CFLAGS) src/cp.cpp -o bin/cp
+#g++ $(CFLAGS) src/cp.cpp -o bin/cp
+
+ls:
+	g++ $(CFLAGS) src/ls.cpp -o bin/ls -Wno-write-strings
--- src/ls.cpp
+++ src/ls.cpp
@@ -11,8 +11,39 @@
 #include <string.h>
 #include <pwd.h>
 #include <algorithm>
+#include <grp.h>
+#include <sstream>
 
 using namespace std;
+
+struct permissions {
+	permissions() : TYP("-"),
+									O_R("-"),
+									O_W("-"),
+									O_X("-"),
+									G_R("-"),
+									G_W("-"),
+									G_X("-"),
+									A_R("-"),
+									A_W("-"),
+									A_X("-") {}
+
+	char* TYP;
+	char* O_R;
+	char* O_W;
+	char* O_X;
+	char* G_R;
+	char* G_W;
+	char* G_X;
+	char* A_R;
+	char* A_W;
+	char* A_X;
+	
+};
+
+bool charCompare(const char * c1, const char * c2){
+	return strcasecmp(c1, c2) < 0;
+}
 
 //FLAGS
 void not_a_flag (vector<char*> & files){
@@ -46,22 +77,80 @@ void l_flag (vector<char*> & files, char & whitespace) {
 			exit(1);
 		}
 
-		struct passwd* ownerInfo;
 		errno = 0;
 
+		struct passwd* ownerInfo;
+		struct group* groupInfo;
+
 		ownerInfo = getpwuid(buf.st_uid);
+		groupInfo = getgrgid(buf.st_gid);
+
 	
 		if (errno != 0) {
 			perror("There was an error with getpwuid()");
 			exit(1);
 		}
 
-		char* userCopy = (char*)malloc(strlen(ownerInfo->pw_name) + 1);
+
+		permissions p;
+		cout << buf.st_ino << endl;
+		if (buf.st_mode & S_IRUSR) p.O_R = "r";
+		if (buf.st_mode & S_IWUSR) p.O_W = "w";
+		if (buf.st_mode & S_IXUSR) p.O_X = "x";
+		if (buf.st_mode & S_IRGRP) p.G_R = "r";
+		if (buf.st_mode & S_IWGRP) p.G_W = "w";
+		if (buf.st_mode & S_IXGRP) p.G_X = "x";
+		if (buf.st_mode & S_IROTH) p.A_R = "r";
+		if (buf.st_mode & S_IWOTH) p.A_W = "w";
+		if (buf.st_mode & S_IXOTH) p.A_X = "x";
+
+		char* permlist = (char*)malloc(255);
+
+		strcpy(permlist, p.TYP);
+		strcat(permlist, p.O_R);
+		strcat(permlist, p.O_W);
+		strcat(permlist, p.O_X);
+		strcat(permlist, p.G_R);
+		strcat(permlist, p.G_W);
+		strcat(permlist, p.G_X);
+		strcat(permlist, p.A_R);
+		strcat(permlist, p.A_W);
+		strcat(permlist, p.A_X);
+
+		char* userCopy = (char*)malloc(strlen(ownerInfo->pw_name) + 1); 
 		strcpy(userCopy, ownerInfo->pw_name);
 
-		files.push_back(userCopy);
-		strcat(files.back(), " ");
+		char* grpCopy = (char*)malloc(strlen(groupInfo->gr_name) + 1);
+		strcpy(grpCopy, groupInfo->gr_name);
+
+		stringstream strs;
+		strs << buf.st_size;
+		string fsize = strs.str();
+		char* fsizeC = (char*) fsize.c_str();
+
+		struct tm * tm;
+		char datetime[200];
+		time_t temp = buf.st_mtime;
+		tm = localtime(&temp);
+		strftime(datetime, sizeof(datetime), "%b %d %H:%M", tm);
+
+		files.push_back(permlist);
+
+		strcat(files.back(), "\t");
+		strcat(files.back(), userCopy);
+
+		strcat(files.back(), "\t");
+		strcat(files.back(), grpCopy);
+
+		strcat(files.back(), "\t");
+		strcat(files.back(), fsizeC);
+
+		strcat(files.back(), "\t");
+		strcat(files.back(), datetime);
+
+		strcat(files.back(), "\t");
 		strcat(files.back(), *i);
+
 
 
 	}
@@ -73,6 +162,7 @@ void l_flag (vector<char*> & files, char & whitespace) {
 //MAIN
 int main(int argc, char* argv[]){
 	bool a = 0, l = 0, R = 0;
+	if (R); //temporary
 	char whitespace = ' ';
 
 
@@ -107,6 +197,9 @@ int main(int argc, char* argv[]){
 
 	//CONDITIONAL ADJUSTMENTS
 	if (!a) not_a_flag(filenames); //no -a
+
+
+	sort(filenames.begin(), filenames.end(), charCompare);
 	
 	if (l) l_flag(filenames, whitespace);
 
@@ -123,6 +216,7 @@ int main(int argc, char* argv[]){
 		perror("There was an error with closedir()");
 		exit(1);
 	}
+
 
 	return 0;
 }
